3d2e1a9582d032fe5ba59fcb3b3587f3f4a50f74
Eshan Ranasinghe <it24100900@my.sliit.lk>
2026-05-17 01:40:33 +0530

    Implement EmailService for sending reminders

---
+++ backend/src/main/java/com/autotrack/service/EmailService.java
@@ -0,0 +1,23 @@
+package com.autotrack.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Service;
+
+@Service
+public class EmailService {
+    @Autowired private JavaMailSender mailSender;
+
+    public void sendReminder(String to, String subject, String body) {
+        try {
+            SimpleMailMessage msg = new SimpleMailMessage();
+            msg.setTo(to);
+            msg.setSubject(subject);
+            msg.setText(body);
+            mailSender.send(msg);
+        } catch (Exception e) {
+            throw new RuntimeException("Failed to send email: " + e.getMessage());
+        }
+    }
+}
